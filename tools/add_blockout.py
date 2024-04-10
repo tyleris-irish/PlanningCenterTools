8ebc216d82d4a8d7aac04cae7620d5846363ae5e
Tyler Ryan <46329645+tyleris-irish@users.noreply.github.com>
2024-04-10 12:48:02 +0000

    Refactor code for greater readability

--- tools/add_blockout.py
+++ tools/add_blockout.py
@@ -2,45 +2,51 @@ from setup import pick_context, pick_names, get_blockout_info
 from api_pco import PCOContext, services_get_all_people, services_add_blockout
 
 def main():
+    """
+    Adds blockout dates for individuals in Planning Center Services based on provided names and blockout information.
+    
+    Retrieves application context and fetches all people from the services. It attempts to match each person with
+    the given names list and adds blockout periods if matches are found. Outputs a message for any names without
+    matching records or missing blockout information.
+    
+    Returns:
+        None
+    """
     context = pick_context()
     names = pick_names()
     blockouts = get_blockout_info()
     pco = PCOContext(context['application_id'], context['secret'], context['organization_id'])
     all_people = services_get_all_people(pco)
 
-    for missionary in names:
-        missionary['First Name'] = missionary['Full Name'].split(' ')[0]
-        missionary['Last Name'] = missionary['Full Name'].split(' ')[-1]
-        trip = missionary['Trip']
+    for blockout_user in names:
+        blockout_user['First Name'] = blockout_user['Full Name'].split(' ')[0]
+        blockout_user['Last Name'] = blockout_user['Full Name'].split(' ')[-1]
+        trip = blockout_user['Trip']
         if trip not in blockouts:
             print(f'No blockout information found for {trip}')
             continue
 
         match_found = False
         for page in all_people:
-            for volunteer in page['data']:
-                if missionary['Full Name'] == volunteer['attributes']['full_name']:
-                    #print(f'Found a Full Name match for {missionary["Full Name"]}')
-                    services_add_blockout(pco, volunteer['id'], blockouts[trip]['starts_at'], blockouts[trip]['ends_at'], blockouts[trip]['reason'])
-                    match_found = True
-                    break
-                elif missionary['First Name'] == volunteer['attributes']['first_name'] and missionary['Last Name'] == volunteer['attributes']['last_name']:
-                    #print(f'Found a First Name and Last Name match for {missionary["Full Name"]}')
-                    services_add_blockout(pco, volunteer['id'], blockouts[trip]['starts_at'], blockouts[trip]['ends_at'], blockouts[trip]['reason'])
-                    match_found = True
-                    break
-                elif missionary['First Name'] == volunteer['attributes']['full_name'].split(' ')[0] and missionary['Last Name'] == volunteer['attributes']['full_name'].split(' ')[-1]:
-                    #print(f'Found a unique First Name and Last Name match for {missionary["Full Name"]}')
-                    services_add_blockout(pco, volunteer['id'], blockouts[trip]['starts_at'], blockouts[trip]['ends_at'], blockouts[trip]['reason'])
-                    match_found = True
-                    break
-            if match_found:
-                break
-        if not match_found:
-            print(f'No match found for {missionary["Full Name"]}')
+           for services_user in page['data']:
+               if blockout_user['Full Name'] == services_user['attributes']['full_name']:
+                   #print(f'Found a Full Name match for {missionary["Full Name"]}')
+                   services_add_blockout(pco, services_user['id'], blockouts[trip]['starts_at'], blockouts[trip]['ends_at'], blockouts[trip]['reason'])
+                   match_found = True
+               elif blockout_user['First Name'] == services_user['attributes']['first_name'] and blockout_user['Last Name'] == services_user['attributes']['last_name']:
+                   #print(f'Found a First Name and Last Name match for {missionary["Full Name"]}')
+                   services_add_blockout(pco, services_user['id'], blockouts[trip]['starts_at'], blockouts[trip]['ends_at'], blockouts[trip]['reason'])
+                   match_found = True
+               elif blockout_user['First Name'] == services_user['attributes']['full_name'].split(' ')[0] and blockout_user['Last Name'] == services_user['attributes']['full_name'].split(' ')[-1]:
+                   #print(f'Found a unique First Name and Last Name match for {missionary["Full Name"]}')
+                   services_add_blockout(pco, services_user['id'], blockouts[trip]['starts_at'], blockouts[trip]['ends_at'], blockouts[trip]['reason'])
+                   match_found = True
+               if match_found:
+                   break
+           if match_found:
+               break
+        else:
+           print(f'No match found for {blockout_user["Full Name"]}')
         
-
-
-
 if __name__ == '__main__':
     main()
--- tools/setup.py
+++ tools/setup.py
@@ -1,16 +1,20 @@
-import glob
+from pathlib import Path
 import json
 import csv
 
 def grab_all_contexts() -> dict:
+    """
+    Grab all contexts from the context directory and return them as a dictionary.
+
+    Returns:
+        dict: A dictionary containing the contexts, where the keys are the file names and the values are the context contents.
+    """
     contexts = {}
-    # navigate to ./context and grab each .cxt file
-    for cxt in glob.glob('context/*.cxt'):
-        # open the file and read the contents
+    for cxt in Path('context/*.cxt'):
         with open(cxt, 'r') as json_file:
             context = json.load(json_file)
-            file_name = cxt.split('/')[-1].split('.')[0]
-            contexts[file_name] = context
+        file_name = cxt.split('/')[-1].split('.')[0]
+        contexts[file_name] = context
     
     if not contexts:
         print('No contexts found. Please create a {name}.cxt file in the context directory.')
@@ -20,10 +24,16 @@ def grab_all_contexts() -> dict:
     return contexts
 
 def pick_context() -> dict:
+    """
+    Prompts the user to choose a context from a list of available contexts.
+
+    Returns:
+        dict: The selected context.
+    """
     contexts = grab_all_contexts()
     print('Choose a context to use:')
-    for i, context in enumerate(contexts):
-        print(f'{i+1}. {context}')
+    for i, context in enumerate(contexts, 1):
+        print(f'{i}. {context}')
     choice = input('Enter the number of the context you want to use: ')
     try:
         return contexts[list(contexts.keys())[int(choice)-1]]
@@ -32,24 +42,32 @@ def pick_context() -> dict:
         return pick_context()
     
 def get_blockout_info() -> dict:
-    blockouts_file = 'input/blockouts.json'
-    with open(blockouts_file, 'r') as json_file:
-        blockouts = json.load(json_file)
-    return blockouts
+    """
+    Retrieves blockout information from a JSON file.
+
+    Returns:
+        A dictionary containing the blockout information.
+    """
+    return json.load(Path('input/blockouts.json').read_text())
 
 def grab_all_names() -> dict:
+    """
+    Grab all names from CSV files in the 'input' directory and return them as a dictionary.
+
+    Returns:
+        dict: A dictionary containing the names from each CSV file, where the keys are the file names and the values are the data from the CSV files.
+    """
     name_files = {}
     # navigate to ./names and grab each csv file
-    for name_file in glob.glob('input/*.csv'):
-        # open the file and read the contents
+    for name_file in Path('input/*.csv'):
         with open(name_file, 'r') as csv_file:
-            # convert file to json
             file = csv_file.read()
-            csv_dict = csv.DictReader(file.splitlines())
-            data = [row for row in csv_dict]
-            # get file name
-            file_name = name_file.split('/')[-1].split('.')[0]
-            name_files[file_name] = data
+        # convert file to a dictionary
+        csv_dict = csv.DictReader(file.splitlines())
+        data = [row for row in csv_dict]
+        # get file name
+        file_name = name_file.split('/')[-1].split('.')[0]
+        name_files[file_name] = data
     
     if not name_files:
         print('No names found. Please create a {name}.csv file in the names directory.')
@@ -59,6 +77,13 @@ def grab_all_names() -> dict:
     return name_files
 
 def pick_names() -> str:
+    """
+    Prompts the user to choose a names file and returns the selected file.
+
+    Returns:
+        str: The selected names file.
+
+    """
     names = grab_all_names()
     print('Choose a names file to use:')
     for i, name in enumerate(names):
@@ -73,3 +98,5 @@ def pick_names() -> str:
 
 if __name__ == '__main__':
     print(pick_context())
+    print(pick_names())
+    print(get_blockout_info())
